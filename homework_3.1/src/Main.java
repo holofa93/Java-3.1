9883b19407419f1657e385b656bd91e5da1786a2
Neto test <vovka9313@mail.ru>
2023-05-26 22:20:13 +0300

    homework

---
+++ homework_3.1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int x = 25000; //стоимость билета на самолёт
+        int y = 20; //сумма, получаемая за одну милю
+
+        System.out.printf("Количество бонусных миль: " + x / y);
+
+
+
+
+    }
+}
